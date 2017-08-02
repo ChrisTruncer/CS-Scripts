8d21b86991861733f13c32840cc00d697bfa0574
Chris Truncer <CTruncer@christophertruncer.com>
2017-08-02 16:43:58 -0600

    Updated debian script

--- CSDebianInstall.sh
+++ CSDebianInstall.sh
@@ -5,9 +5,7 @@
 
 sudo apt-get update
 sudo apt-get install build-essential
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
-apt-get update
-# Java 7
-apt-get install oracle-java7-installer
+sudo apt-get install software-properties-common
+sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
