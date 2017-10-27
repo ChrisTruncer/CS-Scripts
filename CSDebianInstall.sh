0b7b5a2bc07271ac516c4d01f41f311975dcb937
Chris Truncer <CTruncer@christophertruncer.com>
2017-10-26 21:27:22 -0600

    Updated debian script

--- CSDebianInstall.sh
+++ CSDebianInstall.sh
@@ -4,8 +4,9 @@
 # Cobalt Strike on an Ubuntu 14.04 x64 system
 
 sudo apt-get update
-sudo apt-get install build-essential
+sudo apt-get install build-essential dirmngr
 sudo apt-get install software-properties-common
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C2518248EEA14886
 sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
