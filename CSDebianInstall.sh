469016545a305baf5368a3f76ea7006462647382
Chris Truncer <CTruncer@christophertruncer.com>
2018-02-12 13:26:01 -0700

    Removed sudo command, run with sudo or as root instead

--- CSDebianInstall.sh
+++ CSDebianInstall.sh
@@ -3,9 +3,9 @@
 # This script will install the dependencies needed for
 # Cobalt Strike on an Ubuntu 14.04 x64 system
 
-sudo apt-get update
-sudo apt-get install build-essential
-sudo apt-get install software-properties-common
-sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
+apt-get update
+apt-get install build-essential
+apt-get install software-properties-common
+add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
+apt-get update
+apt-get install oracle-java8-installer
